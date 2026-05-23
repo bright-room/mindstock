db6b8949a121372b6301025db3801662082ba814
kukv <koki-nonaka@outlook.jp>
2026-05-23 17:50:02 +0900

    refactor: drop kotlinx-rpc plugin from :shared (now lives in :rpc)

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("net.brightroom.mindstock.kmp-shared")
     id("org.jetbrains.kotlin.plugin.serialization")
-    id("org.jetbrains.kotlinx.rpc.plugin")
 }
 
 kotlin {
@@ -11,8 +10,6 @@ kotlin {
                 implementation(libs.kotlinx.coroutines.core)
                 implementation(libs.kotlinx.serialization.json)
                 implementation(libs.kotlinx.datetime)
-                implementation(libs.kotlinx.rpc.core)
-                implementation(libs.kotlinx.rpc.serialization.json)
             }
         }
     }
