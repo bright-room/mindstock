84854e00b90b39851bcda20a74b4bfcadbeabe19
kukv <koki-nonaka@outlook.jp>
2026-06-06 17:27:07 +0900

    feat(frontend): add ToastController single-channel toast state

---
+++ frontend/src/commonTest/kotlin/net/brightroom/mindstock/frontend/core/ui/ToastControllerTest.kt
@@ -0,0 +1,18 @@
+package net.brightroom.mindstock.frontend.core.ui
+
+import io.kotest.matchers.shouldBe
+import mindstock.frontend.generated.resources.Res
+import mindstock.frontend.generated.resources.toast_replenished
+import kotlin.test.Test
+
+class ToastControllerTest {
+    @Test
+    fun show_publishes_message_and_dismiss_clears() {
+        val controller = ToastController()
+        controller.current.value shouldBe null
+        controller.show(UiText(Res.string.toast_replenished))
+        controller.current.value?.text?.resource shouldBe Res.string.toast_replenished
+        controller.dismiss()
+        controller.current.value shouldBe null
+    }
+}
